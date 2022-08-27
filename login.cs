0d49d824b7d3376640527a274801e919a6795950
Lina Espaillat <likeni@hotmail.com>
2022-08-27 20:41:31 +0200

    add archivo login

---
+++ login.cs
@@ -0,0 +1,5 @@
+//El json web toquen es en la vida real como una licencia de conducir
+// jwt, nos permite transferrir información entre dos partes de manera segura,
+// la idea es que vamos a tener un token que es un string de manera segura
+//vamos a tenerr información a cerca del usuario que se ha autenticado en tu aplicación
+//posteriormente este usuario podra usar este token.  El token tendra un conjunto nombres y valores
